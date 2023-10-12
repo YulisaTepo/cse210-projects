11afb5824732ca3c2962ed5a8947875de5c6b009
Yulisa Alexandra Tepo Cubas <ytepo055@gmail.com>
2023-10-11 22:23:28 -0500

    Learning activity week 5.

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,60 @@
+
+using System.Diagnostics;
+
+class Fraction
+{
+    //Attributes
+    private int _top;
+    private int _bottom;
+
+    //Constructors
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top= wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    //Getters and setters
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom( int bottom)
+    {
+        _bottom = bottom;
+    }
+    
+    //Return methods
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+    public double GetDecimalValue()
+    {
+        double decimalNumber = (double)_top / (double)_bottom;
+        return decimalNumber;
+    }
+    
+}
